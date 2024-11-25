5c331b8d662ec0461ac363ed0cfdbe32a4574d01
TewodrosAdimas <yosefenadagim@gmail.com>
2024-11-25 09:36:56 +0300

    environment set up and user authentications done

---
+++ backend/main/blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('bio', 'profile_picture', 'followers')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('bio', 'profile_picture')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
